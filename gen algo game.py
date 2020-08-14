8112f98334d1d84826d03d4d115f80e6f3d34ddd
ZacharyGott <zachary1002huion@gmail.com>
2020-08-14 00:30:32 -0400

    Replaced main & General Update
    Replace py file and added sensors

---
+++ gen algo game.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+# Packages and dependancies
+
+import math
+import pygame
+pygame.init()
+
+
+# In[10]:
+
+
+# Basic Pygame attributes
+
+size = width, height = 400, 600
+
+black = 0, 0, 0
+white = 255, 255, 255
+
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+
+# In[11]:
+
+
+# Useful functions for Walls class
+
+def horizontalPoint(side):
+    
+    # Returns list of points on side
+    
+    tempList = []
+    
+    tempX = side[0][0]
+    
+    for i in range(side[0][0], side[1][0] + 1):
+        
+        tempList.insert(len(tempList), (tempX, side[0][1]))
+        tempX += 1
+    
+    return tempList
+
+
+def verticlePoint(side):
+    
+    # Returns list of points on side
+    
+    tempList = []
+    
+    tempY = side[0][1]
+    
+    for i in range(side[0][1], side[1][1] + 1):
+        
+        tempList.insert(len(tempList), (side[0][0], tempY))
+        tempY += 1
+    
+    return tempList
+
+
+# In[33]:
+
+
+# Class for creating walls, obbjective and anything related
+
+targetPos = ()
+
+walls = []
+
+class Wall(object):
+
+    def __init__(self, pos):
+        walls.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 40, 40)
+        
+        self.xCent = pos[0]+19
+        self.yCent = pos[1]+19
+        
+        self.UL_corner = (self.xCent-15, self.yCent+15)
+        self.UR_corner = (self.xCent+15, self.yCent+15)
+        self.BR_corner = (self.xCent+15, self.yCent-15)
+        self.BL_corner = (self.xCent-15, self.yCent-15)
+        
+        self.top = (self.UL_corner, self.UR_corner)
+        self.left = (self.BL_corner, self.UL_corner)
+        self.bottom = (self.BL_corner, self.BR_corner)
+        self.right = (self.BR_corner, self.UR_corner)
+        
+        self.topList = horizontalPoint(self.top)
+        self.bottomList = horizontalPoint(self.bottom)
+        self.leftList = verticlePoint(self.left)
+        self.rightList = verticlePoint(self.right)
+        
+        
+wallString = [
+    'WWWWWWWWWW',
+    'W   E    W',
+    'W        W',
+    'W  WW WW W',
+    'W  W     W',
+    'W        W',
+    'W WW WWW W',
+    'W      W W',
+    'W        W',
+    'W        W',
+    'W        W',
+    'W WW  WW W',
+    'WW       W',
+    'WW       W',
+    'WWWWWWWWWW'
+]
+
+dx = dy = 0
+for row in wallString:
+    for col in row:
+        if col == "W":
+            Wall((dx, dy))
+        if col == "E":
+            end_rect = pygame.Rect(dx + 20, dy, 40, 40)
+            targetPos = (dx + 20, dy)
+        dx += 40
+    dy += 40
+    dx = 0
+
+
+# In[122]:
+
+
+# Functions for the "Player" class to fill in data
+    
+
+def midpoint(side):
+    
+    # Finds midpoint using midpoint formula and returns as (x, y)
+    
+    return (round((side[0][0] + side[1][0])/2), round((side[0][1] + side[1][1])/2))
+
+
+def distLeft(middlePoint, walls):
+    
+    # Returns the distance to the nearest obstruction on a given side
+    
+    listOfCoords = []
+
+    for i in walls:
+        for n in i.rightList:
+            if middlePoint[1]-20 < n[1] < middlePoint[1]+20 and n[0] < middlePoint[0]:
+                listOfCoords.insert(len(listOfCoords), n)
+    nearest = max(listOfCoords)
+    
+    return round(math.sqrt(((nearest[0] - middlePoint[0]) ** 2) + ((nearest[1] - middlePoint[1]) ** 2)))
+
+
+def distFront(middlePoint, walls):
+    
+    # Returns the distance to the nearest obstruction on a given side
+    
+    listOfCoords = []
+    for i in walls:
+        for n in i.topList:
+
+            if middlePoint[0]-20 < n[0] < middlePoint[0]+20 and n[1] < middlePoint[1]:
+                listOfCoords.insert(len(listOfCoords), n)
+    nearest = max(listOfCoords)
+    
+    return round(math.sqrt(((nearest[0] - middlePoint[0]) ** 2) + ((nearest[1] - middlePoint[1]) ** 2)))
+
+
+def distRight(middlePoint, walls):
+    
+    # Returns the distance to the nearest obstruction on a given side
+    
+    listOfCoords = []
+
+    for i in walls:
+        for n in i.rightList:
+            if middlePoint[1]-20 < n[1] < middlePoint[1]+20 and n[0] > middlePoint[0]:
+                listOfCoords.insert(len(listOfCoords), n)
+    nearest = min(listOfCoords)
+    
+    return round(math.sqrt(((nearest[0] - middlePoint[0]) ** 2) + ((nearest[1] - middlePoint[1]) ** 2)))
+
+
+def distObj(selfCenter, targetCenter):
+    
+    # Returns distance between self and the middle of the target
+    
+    return round(math.sqrt(((targetCenter[0] - selfCenter[0]) ** 2) + ((targetCenter[1] - selfCenter[1]) ** 2)))
+
+
+# In[123]:
+
+
+# Creates a player class with useful data
+
+
+class Player():
+    
+    model = pygame.image.load("spaceship.png")
+    sprite = pygame.transform.scale(model, (30, 30))
+    
+    chromosome = []
+    
+    xChange = 0
+    yChange = 0
+    
+    xPos = (width * .5) - 15
+    yPos = (height * .8) + 15
+    
+    xCent = xPos + 15
+    yCent = yPos + 15
+    selfCenter = (xCent, yCent)
+    
+    UL_corner = (xCent-15, yCent+15)
+    UR_corner = (xCent+15, yCent+15)
+    BR_corner = (xCent+15, yCent-15)
+    BL_corner = (xCent-15, yCent-15)
+    
+    top = (UL_corner, UR_corner)
+    left = (BL_corner, UL_corner)
+    bottom = (BL_corner, BR_corner)
+    right = (BR_corner, UR_corner)
+    
+    lMid = midpoint(left)
+    tMid = midpoint(top)
+    rMid = midpoint(right)
+    
+    sensLeft = distLeft(lMid, walls)
+    sensFront = distFront(tMid, walls)
+    sensRight = distRight(rMid, walls)
+    sensObj = distObj(selfCenter, targetPos)
+    
+    
+    
+    fitness = 0
+
+
+# In[124]:
+
+
+# Initialize bots and related criteria
+
+
+bot1 = Player()
+
+bot2 = Player()
+
+bot3 = Player()
+
+bot4 = Player()
+
+bot5 = Player()
+
+bot6 = Player()
+
+bot7 = Player()
+
+bot8 = Player()
+
+
+botList = [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8]
+
+
+# In[125]:
+
+
+# Function for the game loop
+
+def gameloop():
+    
+    #function containing the loop for the main processes
+    
+    pass
+
+
+# In[128]:
+
+
+bot1.sensLeft
+
+
+# In[ ]:
+
+
+
+
